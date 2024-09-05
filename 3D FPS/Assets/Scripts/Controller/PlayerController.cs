928bf6d193aae1314f7a2cd64fbf133048006aef
JiroudiR <ryan.ji8630@vusd.us>
2024-09-05 15:33:10 -0700

    Desktop 9/5
    Finished Level1Design1-2 and PlayerController1-2. Working on PlayerController3 (4:17).

--- 3D FPS/Assets/Scripts/Controller/PlayerController.cs
+++ 3D FPS/Assets/Scripts/Controller/PlayerController.cs
@@ -7,7 +7,22 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour
 {
+    [Header("Settings")]
+    [Tooltip("The speed at which the player moves")]
+    public float moveSpeed = 2f;
+    [Tooltip("The speed at which the player rotates to look left and right (calculated in degrees)")]
+    public float lookSpeed = 60f;
+    [Tooltip("The power with which the player jumps")]
+    public float jumpPower = 8f;
+    [Tooltip("The strength of gravity")]
+    public float gravity = 9.81f;
+    [Header("Required References")]
+    [Tooltip("The player shooter script that fires projectiles")]
     public Shooter playerShooter;
+
+    // The character controller component on the player
+    private CharacterController controller;
+    private InputManager inputManager;
 
     /// <summary>
     /// Description:
@@ -19,7 +34,22 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Start()
     {
+        SetUpCharacterController();
+        SetUpInputManager();
+    }
 
+    private void SetUpCharacterController()
+    {
+        controller = GetComponent<CharacterController>();
+        if (controller == null)
+        {
+            Debug.LogError("The player controller script does not have a character controller on the same game object!");
+        }
+    }
+
+    void SetUpInputManager()
+    {
+        inputManager = InputManager.instance;
     }
 
     /// <summary>
@@ -32,6 +62,13 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
+        ProcessMovement();
+    }
 
+    void ProcessMovement()
+    {
+        // Get the input from the input manager
+        float leftRightInput = inputManager.horizontalMoveAxis;
+        Debug.Log("The left right input value is: " + leftRightInput);
     }
 }
