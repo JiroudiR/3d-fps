6a0ea211d3b61761d968ec01ae93b10c6968c90e
JiroudiR <ryan.ji8630@vusd.us>
2024-09-06 15:31:10 -0700

    Desktop 9/6
    Finished PlayerController3-4, PlayerPrefab, EnemyAI1-2, and FinishLevel. Working on CreatingAnotherLevel (2:23).

--- 3D FPS/Assets/Scripts/Controller/PlayerController.cs
+++ 3D FPS/Assets/Scripts/Controller/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 /// <summary>
 /// This class handles the movement of the player with given input from the input manager
@@ -16,9 +17,17 @@ public class PlayerController : MonoBehaviour
     public float jumpPower = 8f;
     [Tooltip("The strength of gravity")]
     public float gravity = 9.81f;
+
+    [Header("Jump Timing")]
+    public float jumpTimeLeniency = 0.1f;
+    float timeToStopBeingLenient = 0;
+
     [Header("Required References")]
     [Tooltip("The player shooter script that fires projectiles")]
     public Shooter playerShooter;
+    public Health playerHealth;
+    public List<GameObject> disableWhileDead;
+    bool doubleJumpAvailable = false;
 
     // The character controller component on the player
     private CharacterController controller;
@@ -62,13 +71,82 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void Update()
     {
+        if (playerHealth.currentHealth <= 0)
+        {
+            foreach (GameObject inGameObject in disableWhileDead)
+            {
+                inGameObject.SetActive(false);
+            }
+            return;
+        }
+        else
+        {
+            foreach (GameObject inGameObject in disableWhileDead)
+            {
+                inGameObject.SetActive(true);
+            }
+        }
+
         ProcessMovement();
+        ProcessRotation();
     }
+
+    Vector3 moveDirection;
 
     void ProcessMovement()
     {
         // Get the input from the input manager
         float leftRightInput = inputManager.horizontalMoveAxis;
-        Debug.Log("The left right input value is: " + leftRightInput);
+        float forwardBackwardInput = inputManager.verticalMoveAxis;
+        bool jumpPressed = inputManager.jumpPressed;
+
+        // Handle the control of the player while it is on the ground
+        if (controller.isGrounded)
+        {
+            doubleJumpAvailable = true;
+            timeToStopBeingLenient = Time.time + jumpTimeLeniency;
+
+            // Set the movement direction to be the received input, set y to 0 since we are on the ground
+            moveDirection = new Vector3(leftRightInput, 0, forwardBackwardInput);
+            // Set the move direction in relation to the transform
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * moveSpeed;
+
+            if (jumpPressed)
+            {
+                moveDirection.y = jumpPower;
+            }
+        }
+        else
+        {
+            moveDirection = new Vector3(leftRightInput * moveSpeed, moveDirection.y, forwardBackwardInput * moveSpeed);
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            if (jumpPressed && Time.time < timeToStopBeingLenient)
+            {
+                moveDirection.y = jumpPower;
+            }
+            else if (jumpPressed && doubleJumpAvailable)
+            {
+                moveDirection.y = jumpPower;
+                doubleJumpAvailable = false;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        if (controller.isGrounded && moveDirection.y < 0)
+        {
+            moveDirection.y = -0.3f;
+        }
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void ProcessRotation()
+    {
+        float horizontalLookInput = inputManager.horizontalLookAxis;
+        Vector3 playerRotation = transform.rotation.eulerAngles;
+        transform.rotation = Quaternion.Euler(new Vector3(playerRotation.x, playerRotation.y + horizontalLookInput * lookSpeed * Time.deltaTime, playerRotation.z));
     }
 }
